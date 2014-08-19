9361cf4fed10ba28937d956cd8a487a2fa69df13
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-08-18 22:29:35 -0500

    cram field in list

--- client/views/list.js
+++ client/views/list.js
@@ -1,12 +1,18 @@
 /*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
 newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true,
 white:true*/
-/*global XT:true, XM:true, Backbone:true, enyo:true, console:true */
+/*global XT:true, XM:true, XV:true, enyo:true, console:true */
 
 (function () {
   "use strict";
 
   XT.extensions.xtuple_field_dev_sample.initList = function () {
-    console.log("hi there");
+
+    var oldProspectListCreate = XV.ProspectList.prototype.create;
+    XV.ProspectList.prototype.create = function () {
+      oldProspectListCreate.apply(this, arguments);
+      console.log(this.$); // (debug code: to inspect the list components)
+      this.createComponent({kind: "XV.ListAttr", attr: "alternateNumber", container: this.$.listColumn2});
+    };
   };
 }());
